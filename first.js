b105e201455c8ae97b6caaa15dc8af929bc7f9e2
avishkar <avishkartapkir2635@gmail.com>
2024-12-10 18:47:45 +0530

    update first.js

--- first.js
+++ first.js
@@ -223,14 +223,47 @@
 //     console.log(a+b);
 // }
 
-// Vowel practise 
+// Vowel practice 
 
-function countVowel(str){
-    let count=0;
-    for(const char of str){
-        if(char==="a"|| char==="e"||char==="i"||char==="o"||char==="u"){
-            count ++;
-        }
-    }
-    return count;
-}
+// function countVowel(str){
+//     let count=0;
+//     for(const char of str){
+//         if(char==="a"|| char==="e"||char==="i"||char==="o"||char==="u"){
+//             count ++;
+//         }
+//     }
+//     return count;
+// }
+
+
+// forEach loop in array
+
+let arr=[1,2,3,4,5];
+arr.forEach((valu) => {
+    console.log(valu);
+});
+
+// practice 
+
+let num1 =[1,2,3,4,5,6];
+
+num1.forEach((vak)=>{
+    console.log(vak*vak);
+    
+})
+
+// map method 
+
+let nums=[1,2,3,4,5,6,7];
+let newarr=nums.map((val)=>{
+    return val*val;
+})
+
+console.log(newarr);
+
+//filter method 
+
+let newarray=nums.filter((val1)=>{
+    return val1%2==0;
+})
+console.log(newarray);
